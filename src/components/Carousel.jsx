06136a19094daa0f97fe5f9d622d5a3195134ef6
FadhilKholaf <fadhilgd123@gmail.com>
2023-11-12 12:08:22 +0700

    fix: eror scrollspy on phone

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,24 +4,23 @@ import Button from "./Button";
 import NavItem from "./NavItem";
 
 const Carousel = () => {
-  const [activeSection, setActiveSection] = useState();
+
+  const [activeSection, setActiveSection] = useState(null);
 
   useEffect(() => {
     const handleScroll = () => {
       const sections = document.querySelectorAll(".scroll-spy-section");
       sections.forEach((section) => {
         const rect = section.getBoundingClientRect();
-        if (useState === null) {
-          setActiveSection("Experience");
-        } else if (rect.top <= 50 && rect.bottom >= 50) {
+        if (rect.top <= 50 && rect.bottom >= 50) {
           setActiveSection(section.id);
         }
       });
     };
 
-    window.addEventListener("click", handleScroll);
+    window.addEventListener("touchmove", handleScroll);
     return () => {
-      window.removeEventListener("click", handleScroll);
+      window.removeEventListener("touchmove", handleScroll);
     };
   }, []);
 
@@ -32,26 +31,10 @@ const Carousel = () => {
     >
       <div className="p-0">
         <ul className="nav d-flex justify-content-between">
-          <NavItem
-            id="#Skills"
-            title="Skills"
-            active={activeSection === "Skills"}
-          />
-          <NavItem
-            id="#Experience"
-            title="Experience"
-            active={activeSection === "Experience"}
-          />
-          <NavItem
-            id="#Education"
-            title="Education"
-            active={activeSection === "Education"}
-          />
-          <NavItem
-            id="#Certificates"
-            title="Certificates"
-            active={activeSection === "Certificates"}
-          />
+          <NavItem id="#Skills" title="Skills" active={activeSection === "Skills"} />
+          <NavItem id="#Experience" title="Experience" active={activeSection === "Experience"} />
+          <NavItem id="#Education" title="Education" active={activeSection === "Education"} />
+          <NavItem id="#Certificates" title="Certificates" active={activeSection === "Certificates"} />
         </ul>
       </div>
       <div
@@ -60,10 +43,7 @@ const Carousel = () => {
         className="container scrollable-scrollspy overflow-hidden row row-cols-1 border border-warning rounded p-0"
         tabIndex="0"
       >
-        <div
-          id="Skills"
-          className="col min-height px-2 py-2 scroll-spy-section"
-        >
+        <div id="Skills" className="col min-height px-2 py-2 scroll-spy-section">
           <div className="d-flex flex-wrap grid gap-2">
             <Button value="HTML" animate="fade-left" />
             <Button value="CSS" animate="fade-left" delay="100" />
@@ -79,29 +59,20 @@ const Carousel = () => {
             <Button value="Figma" animate="fade-left" delay="700" />
           </div>
         </div>
-        <div
-          id="Experience"
-          className="col px-2 py-2 min-height scroll-spy-section"
-        >
+        <div id="Experience" className="col px-2 py-2 min-height scroll-spy-section">
           <div className="d-flex flex-wrap grid gap-2">
             <Button value="Software Engineering student" />
             <Button value="Video editor freelancer" />
           </div>
         </div>
-        <div
-          id="Education"
-          className="col px-2 py-2 min-height scroll-spy-section"
-        >
+        <div id="Education" className="col px-2 py-2 min-height scroll-spy-section">
           <div className="d-flex flex-wrap grid gap-2">
             <Button value="SDI Al-Munawwar" />
             <Button value="MTsN 1 Tulungagung" />
             <Button value="SMK Telkom Malang" />
           </div>
         </div>
-        <div
-          id="Certificates"
-          className="col px-2 py-2 min-height scroll-spy-section"
-        >
+        <div id="Certificates" className="col px-2 py-2 min-height scroll-spy-section">
           <div className="d-flex flex-wrap grid gap-2">
             <Button value="Kaggle pandas course" />
           </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,7 @@ import Fadhil from "../assets/images/fadhil.jpg";
 const Profile = () => {
   return (
     <div data-aos="flip-up" data-aos-delay="100" className="container d-flex justify-content-center">
-      <img className="rounded shadow-lg img-profile" src={Fadhil} alt={"fadhil.jpg"} />
+      <img className="rounded shadow-lg img-profile" src={Fadhil} alt="fadhil.jpg" />
     </div>
   );
 };
